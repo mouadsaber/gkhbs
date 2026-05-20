d809ee241debea6ac4c1e06d051000f4f44c624b
Mouad <mouad.saber@gmail.com>
2026-05-20 14:45:10 +0100

    Add TikTok Pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const pixelId = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID || "2376048099583195";
+  const tikTokPixelId = process.env.NEXT_PUBLIC_TIKTOK_PIXEL_ID || "D86RK7RC77U6OC336NC0";
   return (
     <html lang="fr" className="antialiased overflow-x-hidden" suppressHydrationWarning>
       <body className="flex flex-col overflow-x-hidden" suppressHydrationWarning>
@@ -28,6 +29,42 @@ export default function RootLayout({
         <WhatsAppHelpFab />
         <MetaPixelDebug />
         <MetaPixelEnsurePageView />
+        {tikTokPixelId ? (
+          <Script
+            id="tiktok-pixel"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+!function (w, d, t) {
+  w.TiktokAnalyticsObject=t;
+  var ttq=w[t]=w[t]||[];
+  ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
+  ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0))) }};
+  for (var i=0;i<ttq.methods.length;i++) ttq.setAndDefer(ttq,ttq.methods[i]);
+  ttq.instance=function(t){for (var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++) ttq.setAndDefer(e,ttq.methods[n]); return e};
+  ttq.load=function(e,n){
+    var i="https://analytics.tiktok.com/i18n/pixel/events.js";
+    ttq._i=ttq._i||{};
+    ttq._i[e]=[];
+    ttq._i[e]._u=i;
+    ttq._t=ttq._t||{};
+    ttq._t[e]=+new Date;
+    ttq._o=ttq._o||{};
+    ttq._o[e]=n||{};
+    var o=d.createElement("script");
+    o.type="text/javascript";
+    o.async=!0;
+    o.src=i+"?sdkid="+e+"&lib="+t;
+    var a=d.getElementsByTagName("script")[0];
+    a.parentNode.insertBefore(o,a);
+  };
+  ttq.load('${tikTokPixelId}');
+  ttq.page();
+}(window, document, 'ttq');
+              `,
+            }}
+          />
+        ) : null}
         {pixelId ? (
           <>
             <Script
